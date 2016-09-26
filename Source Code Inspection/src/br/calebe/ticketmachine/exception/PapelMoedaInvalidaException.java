3f48f0b2c442b005cf04ff8fd8de2eb725d05c6d
palomaaraujo <paloma.araujo6@hotmail.com>
2016-09-26 01:02:13 -0300

    Update PapelMoedaInvalidaException.java

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -4,6 +4,10 @@ package br.calebe.ticketmachine.exception;
  *
  * @author Calebe de Paula Bianchini
  */
-public class PapelMoedaInvalidaException extends Exception {
+public class PapelMoedaInvalidaException extends Exception{
     
+public PapelMoedaInvalidaException(){
+    super("Erro: Nota Inválida!");
+}
+
 }
