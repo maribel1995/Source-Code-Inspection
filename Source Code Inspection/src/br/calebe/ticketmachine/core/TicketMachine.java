84dfbedb3385ca03774e438a90990527b87d905b
palomaaraujo <paloma.araujo6@hotmail.com>
2016-09-26 01:07:12 -0300

    Update TicketMachine.java

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -22,31 +22,37 @@ public class TicketMachine {
     public void inserir(int quantia) throws PapelMoedaInvalidaException {
         boolean achou = false;
         for (int i = 0; i < papelMoeda.length && !achou; i++) {
-            if (papelMoeda[1] == quantia) {
+            if (papelMoeda[i] == quantia) {
                 achou = true;
             }
         }
         if (!achou) {
             throw new PapelMoedaInvalidaException();
-        }
+        } else{
         this.saldo += quantia;
+        }
     }
 
     public int getSaldo() {
         return saldo;
     }
 
-    public Iterator<Integer> getTroco() {
-        return null;
+    public Iterator<PapelMoeda> getTroco() {
+        return (new Troco(saldo)).getIterator();
     }
 
     public String imprimir() throws SaldoInsuficienteException {
-        if (saldo < valor) {
-            throw new SaldoInsuficienteException();
-        }
+        verificaSaldo();
+        
         String result = "*****************\n";
         result += "*** R$ " + saldo + ",00 ****\n";
         result += "*****************\n";
         return result;
+    }
+    
+    public void verificaSaldo() throws SaldoInsuficienteException {
+    if (saldo < valor) {
+            throw new SaldoInsuficienteException();
+        }
     }
 }
